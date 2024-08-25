072e40dcb8ad5f43310f5d946025e5c6edd6fd98
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2024-08-25 19:54:17 +0700

    feat: finish auth API and User API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,18 +2,21 @@ import {
   Controller,
   Get,
   HttpStatus,
+  Param,
   Request,
   UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('user')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get('user/:id')
-  async getUserById(id: string) {
+  async getUserById(@Param('id') id: string) {
     const data = await this.userService.getUserById(id);
     return {
       statusCode: HttpStatus.OK,
